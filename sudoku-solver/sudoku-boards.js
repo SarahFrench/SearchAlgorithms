e75c51f8d25e5e83f279b7a8e0e3d189fcc57a0a
Sarah French <sarah@sarahfrench.co.uk>
2019-07-15 12:28:42 +0100

    New easy board

--- sudoku-solver/sudoku-boards.js
+++ sudoku-solver/sudoku-boards.js
@@ -13,15 +13,15 @@ let assignedBoard = [assignedRow1,assignedRow2,assignedRow3,assignedRow4,assigne
 
 
 // very easy Sudoku
-let easyRow1 = [0,0,8,0,6,9,0,7,2];
-let easyRow2 = [7,6,0,0,0,3,9,1,0];
-let easyRow3 = [0,9,4,7,2,0,3,0,0];
-let easyRow4 = [0,4,0,5,0,7,0,8,3];
-let easyRow5 = [8,0,5,0,0,6,7,0,4];
-let easyRow6 = [6,7,0,8,0,2,0,0,9];
-let easyRow7 = [4,0,0,6,3,0,0,9,1];
-let easyRow8 = [0,8,6,0,5,0,2,4,0];
-let easyRow9 = [9,0,1,2,7,0,8,0,0];
+let easyRow1 = [8,7,6,9,0,0,0,0,0];
+let easyRow2 = [0,1,0,0,0,6,0,0,0];
+let easyRow3 = [0,4,0,3,0,5,8,0,0];
+let easyRow4 = [4,0,0,0,0,0,2,1,0];
+let easyRow5 = [0,9,0,5,0,0,0,0,0];
+let easyRow6 = [0,5,0,0,4,0,3,0,6];
+let easyRow7 = [0,2,9,0,0,0,0,0,8];
+let easyRow8 = [0,0,4,6,9,0,1,7,3];
+let easyRow9 = [0,0,0,0,0,1,0,0,4];
 
 let easyBoard = [easyRow1,easyRow2,easyRow3,easyRow4,easyRow5,easyRow6,easyRow7,easyRow8,easyRow9]
 
